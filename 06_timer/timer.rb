2cc45c82c82fae3ca3145959c85024669be2ba65
bahifi <cantboogethenuge@gmail.com>
2016-10-02 14:33:49 -0500

    working on timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
   #write your code here
+	def seconds
+		@seconds
+	end
+
+	def initialize
+		@seconds = 0
+	end
+
+	def time_string
+		Time.at(@seconds).strftime("%H:%M:%S:%N")
+	end
+
+	def seconds=(secs)
+		@seconds = secs
+	end
+
+
 end
