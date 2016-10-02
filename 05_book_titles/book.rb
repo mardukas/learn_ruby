c4105ea1ace08ee362ee46729f0fb7d4304a243f
bahifi <cantboogethenuge@gmail.com>
2016-10-02 03:17:08 -0500

    finished book title

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
 class Book
 # write your code here
+	def title
+		@title
+	end
+
+	def title=(title)
+		articles = %w{the and of a an in}
+		if title.count(" ") > 0
+			words = title.split(" ")
+			words[0] = words[0].capitalize #Always capitalize the first word
+
+			words.map! do |x|
+				if !(articles.include?(x))
+					x.capitalize
+				else
+					x
+				end
+			end
+
+
+			@title = words.join(' ')
+
+		else 
+			@title = title.capitalize
+		end
+	end
 end
